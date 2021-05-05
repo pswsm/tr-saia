a18130a5bab4225ff26cc2c442f7d89a8b6759cd
pswsm <pauseawesome@protonmail.com>
2021-05-05 21:45:17 +0200

    fast

--- saia/scripts/sidebar.js
+++ saia/scripts/sidebar.js
@@ -1,5 +1,5 @@
 function w3_open() {
-  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("main").style.marginLeft = "0%";
   document.getElementById("mySidebar").style.width = "25%";
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("openNav").style.display = 'none';
