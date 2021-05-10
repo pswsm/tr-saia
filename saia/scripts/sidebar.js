d87e0ed1dce850fa865143507540e4a58e1a87d6
pswsm <pauseawesome@protonmail.com>
2021-05-10 20:30:41 +0200

    nya nyanpasu!

--- saia/scripts/sidebar.js
+++ saia/scripts/sidebar.js
@@ -1,6 +1,6 @@
 function w3_open() {
   document.getElementById("main").style.marginLeft = "0%";
-  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.width = "20%";
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("openNav").style.display = 'none';
 }
